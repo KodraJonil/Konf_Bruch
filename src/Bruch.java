49e0691f72d5a193d4111a68e2ec2ec9154d60e7
s51122 <s51122@edu.campus02.at>
2021-11-07 20:55:21 +0100

    Implementierung von toDecimal

--- src/Bruch.java
+++ src/Bruch.java
@@ -21,12 +21,12 @@ public class Bruch {
     public double toDecimal() {
         // TODO: Die Methode soll den Dezimalwert zum Bruch retournieren
         // Achtung: Ganzzahldivision!
-        return 0;
+        return numerator / denominator;
     }
 
     public String print() {
         // TODO: Die Methode soll den Bruch als Text retournieren z.B.: "5 / 7"
-        return "";
+        return numerator + " / " + denominator;
     }
 
     public Bruch multiplicate(Bruch b2) {
@@ -37,6 +37,8 @@ public class Bruch {
         *   => Selbige auch für denominator
         *  Retournieren Sie ihre neue Bruch-Variable
         * */
+
+        
 
 
         return null;
